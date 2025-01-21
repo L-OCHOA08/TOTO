eade27fbd326d4f4a8c0e0faea13a682160d6b38
L-OCHOA08 <leandroochoagh@gmail.com>
2025-01-21 05:36:21 -0300

    first commit

---
+++ productos/app.js
@@ -0,0 +1,73 @@
+const contenedor = document.querySelector('#productos');
+const filter = document.querySelector('#filter--button');
+const filtros = document.querySelector('.filtros');
+const maximo = document.querySelector('#hasta');
+const filtrarBoton = document.querySelector('#filtros--input');
+
+filtrarBoton.addEventListener('submit', (e)=>{
+    e.preventDefault;
+
+    let resultado = filtros.value;
+    console.log(resultado)
+});
+
+filter.addEventListener('change', ()=>{
+    if (filter.checked) {
+        filtros.classList.add('visible');
+        maximo.style='visibility: visible;';
+        filtrarBoton.style='visibility: visible;';
+    } else{
+        filtros.classList.remove('visible');
+        maximo.style='visibility: hidden;';
+        filtrarBoton.style='visibility: hidden;';
+    }
+})
+
+
+
+document.addEventListener('DOMContentLoaded', cargarProductos(stock));
+
+function cargarProductos(stock) {
+    stock.forEach(stockk => {
+        const { id, marca, modelo, precioMinorista, precioMayorista, img, stock } = stockk;
+
+        if (stock >= 1) {
+            const producto = document.createElement('div');
+            producto.classList.add('product');
+            contenedor.appendChild(producto);
+    
+            const imagenProd = document.createElement('img');
+            imagenProd.classList.add('product--img');
+            imagenProd.src = img
+            producto.appendChild(imagenProd);
+    
+            const contenedorTextos = document.createElement('div');
+            contenedorTextos.classList.add('product--text');
+            producto.appendChild(contenedorTextos);
+    
+            const productoTitulo = document.createElement('h3');
+            productoTitulo.classList.add('product--title');
+            productoTitulo.textContent = marca + ' ' + modelo;
+            contenedorTextos.appendChild(productoTitulo);
+    
+            const precios = document.createElement('div');
+            precios.classList.add('product--price');
+            contenedorTextos.appendChild(precios);
+    
+            const minorista = document.createElement('p');
+            minorista.classList.add('product--price--minorist');
+            minorista.textContent = 'Minorista: ' + precioMinorista;
+            precios.appendChild(minorista);
+    
+            const mayorista = document.createElement('p');
+            mayorista.classList.add('product--price--mayorist');
+            mayorista.textContent = 'Mayorista: ' + precioMayorista;
+            precios.appendChild(mayorista);
+    
+            const agregarBoton = document.createElement('button');
+            agregarBoton.classList.add('product--button');
+            agregarBoton.textContent = 'Agregar';
+            producto.appendChild(agregarBoton);
+        }
+    });
+}
---
+++ productos/bd.js
@@ -0,0 +1,31 @@
+const stock = [
+    {
+        id : 0,
+       marca: 'Samsung',
+       modelo: 'A30',
+       precioMinorista: 50000,
+       precioMayorista: 25000,
+       img : 'https://dcdn.mitiendanube.com/stores/001/897/718/products/celular-samsung-galaxy-a301-ff6627fd268e30ab9716352257436582-640-0.png',
+       stock : 1
+    },
+
+    {
+        id : 1,
+        marca: 'Samsung',
+        modelo: 'A30s',
+        precioMinorista: 70000,
+        precioMayorista: 30000,
+        img: 'https://http2.mlstatic.com/D_NQ_NP_715548-MLA79106307530_092024-O.webp',
+        stock: 5
+    },
+    
+    {
+        id : 2,
+        marca: 'Samsung',
+        modelo: 'J7',
+        precioMinorista: 20000,
+        precioMayorista: 10000,
+        img : 'https://skp.samsungcsportal.com/upload/namo/FAQ/co/20170706/20170706045014427_5B889NFI.png?$ORIGIN_PNG$',
+        stock: 1
+    },
+]
