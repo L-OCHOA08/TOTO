cdfb2a0f16b72f295bf03bf6b38ca1bfa326b7e3
L-OCHOA08 <leandroochoagh@gmail.com>
2025-01-22 22:22:56 -0300

    filter system

--- productos/app.js
+++ productos/app.js
@@ -1,15 +1,22 @@
+// VARIABLES
+
 const contenedor = document.querySelector('#productos');
+const producto = document.querySelectorAll('.product')
 const filter = document.querySelector('#filter--button');
 const filtros = document.querySelector('.filtros');
-const maximo = document.querySelector('#hasta');
 const filtrarBoton = document.querySelector('#filtros--input');
+const maximo = document.querySelector('#hasta');
 
-filtrarBoton.addEventListener('submit', (e)=>{
-    e.preventDefault;
+// EVENT LISTENERS
 
-    let resultado = filtros.value;
-    console.log(resultado)
-});
+
+filtros.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+    filtrarCat();
+})
+
+
 
 filter.addEventListener('change', ()=>{
     if (filter.checked) {
@@ -24,8 +31,12 @@ filter.addEventListener('change', ()=>{
 })
 
 
-
 document.addEventListener('DOMContentLoaded', cargarProductos(stock));
+
+filtrarPrecio()
+
+
+// FUNCIONES
 
 function cargarProductos(stock) {
     stock.forEach(stockk => {
@@ -70,4 +81,112 @@ function cargarProductos(stock) {
             producto.appendChild(agregarBoton);
         }
     });
+}
+
+function filtrarPrecio() {
+
+    let filterMaximo = document.querySelector('#hasta');
+    filterMaximo.addEventListener('keyup', ()=>{
+        let filterMax = document.querySelector('#hasta').value;
+
+    stock.forEach(stockk =>{
+        const { marca, modelo, precioMinorista, precioMayorista, img, categoria} = stockk;
+        if (filterMax >= precioMinorista) {
+            const producto = document.createElement('div');
+            producto.classList.add('product');
+            contenedor.appendChild(producto);
+    
+            const imagenProd = document.createElement('img');
+            imagenProd.classList.add('product--img');
+            imagenProd.src = img
+            producto.appendChild(imagenProd);
+    
+            const contenedorTextos = document.createElement('div');
+            contenedorTextos.classList.add('product--text');
+            producto.appendChild(contenedorTextos);
+    
+            const productoTitulo = document.createElement('h3');
+            productoTitulo.classList.add('product--title');
+            productoTitulo.textContent = marca + ' ' + modelo;
+            contenedorTextos.appendChild(productoTitulo);
+    
+            const precios = document.createElement('div');
+            precios.classList.add('product--price');
+            contenedorTextos.appendChild(precios);
+    
+            const minorista = document.createElement('p');
+            minorista.classList.add('product--price--minorist');
+            minorista.textContent = 'Minorista: ' + precioMinorista;
+            precios.appendChild(minorista);
+    
+            const mayorista = document.createElement('p');
+            mayorista.classList.add('product--price--mayorist');
+            mayorista.textContent = 'Mayorista: ' + precioMayorista;
+            precios.appendChild(mayorista);
+    
+            const agregarBoton = document.createElement('button');
+            agregarBoton.classList.add('product--button');
+            agregarBoton.textContent = 'Agregar';
+            producto.appendChild(agregarBoton);
+        } else{
+            limpiarHTML()
+        }
+    })
+    })
+    
+}
+
+function filtrarCat() {
+    const selector = document.querySelector('#selectorCateg').value;
+
+    stock.forEach(stockk =>{
+        const { marca, modelo, precioMinorista, precioMayorista, img, categoria} = stockk;
+        if (categoria == selector) {
+            const producto = document.createElement('div');
+            producto.classList.add('product');
+            contenedor.appendChild(producto);
+    
+            const imagenProd = document.createElement('img');
+            imagenProd.classList.add('product--img');
+            imagenProd.src = img
+            producto.appendChild(imagenProd);
+    
+            const contenedorTextos = document.createElement('div');
+            contenedorTextos.classList.add('product--text');
+            producto.appendChild(contenedorTextos);
+    
+            const productoTitulo = document.createElement('h3');
+            productoTitulo.classList.add('product--title');
+            productoTitulo.textContent = marca + ' ' + modelo;
+            contenedorTextos.appendChild(productoTitulo);
+    
+            const precios = document.createElement('div');
+            precios.classList.add('product--price');
+            contenedorTextos.appendChild(precios);
+    
+            const minorista = document.createElement('p');
+            minorista.classList.add('product--price--minorist');
+            minorista.textContent = 'Minorista: ' + precioMinorista;
+            precios.appendChild(minorista);
+    
+            const mayorista = document.createElement('p');
+            mayorista.classList.add('product--price--mayorist');
+            mayorista.textContent = 'Mayorista: ' + precioMayorista;
+            precios.appendChild(mayorista);
+    
+            const agregarBoton = document.createElement('button');
+            agregarBoton.classList.add('product--button');
+            agregarBoton.textContent = 'Agregar';
+            producto.appendChild(agregarBoton);
+        } else{
+            limpiarHTML()
+        }
+    })
+}
+
+
+function limpiarHTML() {
+   while (contenedor.firstChild) {
+    contenedor.removeChild(contenedor.firstChild) 
+   }
 }
--- productos/bd.js
+++ productos/bd.js
@@ -1,24 +1,24 @@
 const stock = [
     {
-        id : 0,
-       marca: 'Samsung',
-       modelo: 'A30',
-       precioMinorista: 50000,
-       precioMayorista: 25000,
-       img : 'https://dcdn.mitiendanube.com/stores/001/897/718/products/celular-samsung-galaxy-a301-ff6627fd268e30ab9716352257436582-640-0.png',
-       stock : 1
-    },
-
-    {
         id : 1,
         marca: 'Samsung',
         modelo: 'A30s',
         precioMinorista: 70000,
         precioMayorista: 30000,
         img: 'https://http2.mlstatic.com/D_NQ_NP_715548-MLA79106307530_092024-O.webp',
+        categoria: 'Celulares',
         stock: 5
     },
-    
+    {
+        id : 0,
+       marca: 'Samsung',
+       modelo: 'A30',
+       precioMinorista: 50000,
+       precioMayorista: 25000,
+       img : 'https://dcdn.mitiendanube.com/stores/001/897/718/products/celular-samsung-galaxy-a301-ff6627fd268e30ab9716352257436582-640-0.png',
+       categoria: 'Celulares',
+       stock : 1
+    },
     {
         id : 2,
         marca: 'Samsung',
@@ -26,6 +26,7 @@ const stock = [
         precioMinorista: 20000,
         precioMayorista: 10000,
         img : 'https://skp.samsungcsportal.com/upload/namo/FAQ/co/20170706/20170706045014427_5B889NFI.png?$ORIGIN_PNG$',
+        categoria: 'Celulares',
         stock: 1
     },
 ]
